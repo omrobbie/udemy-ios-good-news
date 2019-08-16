7d460ae38e78dd9d3e70de09d42b14954901b711
omrobbie <putu.roby@yahoo.com>
2019-08-16 14:32:50 +0800

    prepare table view cell

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
