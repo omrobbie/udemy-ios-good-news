e7207aff3bd2b1d4263390b2a5eb0be1a626511e
omrobbie <putu.roby@yahoo.com>
2019-08-16 12:16:43 +0800

    change bar tint color

--- GoodNews/AppDelegate.swift
+++ GoodNews/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 47/255, green: 54/255, blue: 64/255, alpha: 1.0)
+
         return true
     }
 }
