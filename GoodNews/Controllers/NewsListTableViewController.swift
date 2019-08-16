e95dc5fe24c1a5e499d509cf458c9dceaaa96040
omrobbie <putu.roby@yahoo.com>
2019-08-16 15:53:42 +0800

    displaying data to the list

--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
 
+    private var articleListViewModel: ArticleListViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -20,10 +22,35 @@ class NewsListTableViewController: UITableViewController {
 
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=51660c958ad44aeda334a7c53a2e4278")!
 
-        WebService().getArticles(url: url) { (data) in
-            if let data = data {
-                print(data)
+        WebService().getArticles(url: url) { articles in
+            if let articles = articles {
+                self.articleListViewModel = ArticleListViewModel(articles: articles)
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
         }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return articleListViewModel == nil ? 0 : articleListViewModel.numberOfSection
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articleListViewModel.numberOfRowsInSection(section)
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found!")
+        }
+
+        let item = articleListViewModel.articleAtIndex(indexPath.row)
+
+        cell.titleLabel.text = item.title
+        cell.descriptionLabel.text = item.description
+
+        return cell
     }
 }
