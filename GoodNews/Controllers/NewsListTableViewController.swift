17792ffe9baba1220341ff9bf7df2be2fd5a3411
omrobbie <putu.roby@yahoo.com>
2019-08-16 12:05:52 +0800

    add table view controller

---
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
