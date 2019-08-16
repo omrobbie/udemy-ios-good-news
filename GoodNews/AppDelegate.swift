ada110109d8684c4f3301c6205f7f4e0ce93c994
omrobbie <putu.roby@yahoo.com>
2019-08-16 11:56:32 +0800

    Initial Commit

---
+++ GoodNews/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GoodNews
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
